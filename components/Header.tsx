b1ff799f87a3453dd345249ce585c814b1e01fe1
mikey <currygeigei@gmail.com>
2024-07-25 22:22:25 +0800

    feat：設定 Clerk 登入認證

---
+++ components/Header.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+  useUser,
+} from "@clerk/nextjs";
+
+const Header = () => {
+  const { user } = useUser();
+
+  return (
+    <div className="p-5 flex items-center justify-between">
+      {user && (
+        <h1>
+          {user?.firstName}
+          {`'s`} Space
+        </h1>
+      )}
+
+      {/* Breadcrumbs */}
+
+      <div>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
+    </div>
+  );
+};
+export default Header;
