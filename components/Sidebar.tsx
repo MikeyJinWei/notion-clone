7c26002c9fac033d4220dace60f78cd7a6ae5712
mikey <currygeigei@gmail.com>
2024-08-11 18:03:35 +0800

    feat：建置手機版 Sidebar 及按鈕元件

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,44 @@
+import { MenuIcon } from "lucide-react";
 import NewDocumentButton from "./NewDocumentButton";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 
 const Sidebar = () => {
+  const menuOptions = (
+    <>
+      <NewDocumentButton />
+
+      {/* User Documents */}
+      {/* List */}
+
+      {/* Share */}
+      {/* List */}
+    </>
+  );
+
   return (
     <div className="relative p-2 md:p-5 bg-gray-200">
-      <NewDocumentButton />
+      <div className="md:hidden">
+        <Sheet>
+          <SheetTrigger>
+            <MenuIcon className="p-2 rounded-lg hover:opacity-30" size={40} />
+          </SheetTrigger>
+          <SheetContent side="left">
+            <SheetHeader>
+              <SheetTitle>Menu</SheetTitle>
+              <div>{menuOptions}</div>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+      </div>
+
+      <div className="hidden md:inline">{menuOptions}</div>
     </div>
   );
 };
